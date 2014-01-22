dd9f08f245c126b562bfa96526667a261b407517
Neurolit <neurolit@gmail.com>
2014-01-22 10:51:48 +0100

    Index file: tabs->spaces + prepare use of requirejs

---
+++ js/main.js
@@ -0,0 +1,8 @@
+require.config({
+    paths: {
+        jquery: 'vendor/jquery-1.8.2.min',
+        easeljs: 'easeljs-0.5.0.min',
+        jqueryZclip: 'jquery.zclip.min',
+        tweenjs: 'tweenjs-0.3.0.min'
+    }
+});
